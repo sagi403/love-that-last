b114e449c29341c2a5b4c73ebd5317c5aa4fb635
Sagi <sagi403@gmail.com>
2022-12-16 18:49:42 +0200

    Adding profile route to the backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -47,4 +47,23 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { loginUser, registerUser };
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { loginUser, registerUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userControllers.js";
+import {
+  getUserProfile,
+  loginUser,
+  registerUser,
+} from "../controllers/userControllers.js";
 
 const router = express.Router();
 
 router.route("/login").post(loginUser);
 router.route("/register").post(registerUser);
+router.route("/profile").get(getUserProfile);
 
 export default router;
