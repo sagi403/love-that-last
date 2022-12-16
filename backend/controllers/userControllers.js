e552704255a9230302335c75016dbff1725c8d88
Sagi <sagi403@gmail.com>
2022-12-16 18:27:19 +0200

    Adding login and register routes and generateToken util function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,6 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Login user & get token
 // @route   POST /api/users/login
@@ -6,14 +8,43 @@ import asyncHandler from "express-async-handler";
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  if (email === "1" && password === "2") {
-    res.json({
-      msg: "success",
-    });
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({ token: generateToken(user._id) });
   } else {
     res.status(401);
     throw new Error("Invalid email or password");
   }
 });
 
-export { loginUser };
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      token: generateToken(user),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+export { loginUser, registerUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { loginUser } from "../controllers/userControllers.js";
+import { loginUser, registerUser } from "../controllers/userControllers.js";
 
 const router = express.Router();
 
 router.route("/login").post(loginUser);
+router.route("/register").post(registerUser);
 
 export default router;
