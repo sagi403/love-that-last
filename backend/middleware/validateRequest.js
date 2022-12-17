cf3f989369e282d508eb609d07cc5a356fa15539
Sagi <sagi403@gmail.com>
2022-12-17 20:10:03 +0200

    Adding register route tests

--- backend/middleware/validateRequest.js
+++ backend/middleware/validateRequest.js
@@ -4,7 +4,6 @@ const validateRequest = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors);
     res.status(400);
     throw new Error("Invalid information provided");
   }
--- backend/routes/__test__/userRoutes.test.js
+++ backend/routes/__test__/userRoutes.test.js
@@ -1,9 +1,59 @@
 import request from "supertest";
 import { app } from "../../app.js";
 
+// @desc    Register a new user
+// @route   POST /api/users/register
+// @access  Public
+
 it("returns a 201 on successful register", async () => {
   return request(app)
     .post("/api/users/register")
-    .send({ email: "test@test.com", password: "123456", name: "test" })
+    .send({ email: "test@test.com", password: "asdASD123!", name: "test" })
     .expect(201);
+});
+
+it("returns a 400 with an invalid field", async () => {
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: "test@test", password: "asdASD123!", name: "test" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: "test@test.com", password: "123456", name: "test" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: "test@test.com", password: "asdASD123!", name: "t" })
+    .expect(400);
+});
+
+it("returns a 400 with a missing field", async () => {
+  await request(app)
+    .post("/api/users/register")
+    .send({ password: "asdASD123!", name: "test" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: "test@test.com", name: "test" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: "test@test.com", password: "asdASD123!" })
+    .expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: "test@test.com", password: "asdASD123!", name: "test" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/register")
+    .send({ email: "test@test.com", password: "asdASD123!", name: "test" })
+    .expect(400);
 });
