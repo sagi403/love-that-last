52c2795438ae310ecbc0f90a91e68d24ac2c7374
Sagi <sagi403@gmail.com>
2022-12-21 19:30:38 +0200

    Updating the HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -28,15 +28,15 @@ const HomeScreen = () => {
           </p>
         </div>
         <Row className="py-5">
-          <Col className="text-center">
+          <Col sm={12} md={6} className="text-center">
             <img
               className="img-fluid"
               src=".\images\homepage\love-knot-message-card-red-roses.jpg"
               alt="new arrival"
             />
           </Col>
-          <Col className="d-flex align-items-center">
-            <div>
+          <Col sm={12} md={6} className="d-flex align-items-center">
+            <div className="py-5">
               <h6>NEW DESIGN</h6>
               <h2>New Arrival</h2>
               <h5>We are adding new unique design every week</h5>
@@ -46,7 +46,7 @@ const HomeScreen = () => {
         </Row>
         <div className="text-center py-5">
           <h2>Find Something Special</h2>
-          <Button variant="outline-dark" className="my-3">
+          <Button variant="outline-dark" className="my-3 py-1">
             VIEW ALL
           </Button>
           <Row className="py-4">
@@ -55,6 +55,65 @@ const HomeScreen = () => {
                 <Product product={product} />
               </Col>
             ))}
+          </Row>
+        </div>
+      </Container>
+      <div className="newsletter-container">
+        <h3 className="text-light pb-2">
+          Promotions, discounts & more! Sign up now so that you don't miss out:
+        </h3>
+        <div className="d-flex justify-content-center">
+          <input
+            className="newsletter-input"
+            type="text"
+            placeholder="Enter your email"
+          />
+          <Button variant="dark">SUBSCRIBE</Button>
+        </div>
+      </div>
+      <Container className="py-5">
+        <div className="py-5 text-center">
+          <h2>WHY DIAMOND DEN?</h2>
+          <Row className="py-5">
+            <Col sm={12} md={4}>
+              <div>
+                <img
+                  className="img-fluid py-3"
+                  src=".\images\favicon\Secure-Shopping.png"
+                  alt="new arrival"
+                />
+                <h4>Secure Shopping</h4>
+                <p className="d-inline">
+                  When it comes to security, SLEEGG uses best in class
+                  technology so you can rest easy. Your personal details are
+                  never shared, sold or rented to anyone.
+                </p>
+              </div>
+            </Col>
+            <Col sm={12} md={4}>
+              <img
+                className="img-fluid py-3"
+                src=".\images\favicon\Free-shipping.png"
+                alt="new arrival"
+              />
+              <h4>Fast Shipping</h4>
+              <p className="d-inline">
+                With fast shipping, your order will be delivered 2-5 business
+                days after all your items are available to ship.
+              </p>
+            </Col>
+            <Col sm={12} md={4}>
+              <img
+                className="img-fluid py-3"
+                src=".\images\favicon\Unique-Everything.png"
+                alt="new arrival"
+              />
+              <h4>Unique Everything</h4>
+              <p className="d-inline">
+                Here at SLEEGG, All of our items are designed, personalized and
+                fulfilled with love.
+              </p>
+            </Col>
           </Row>
         </div>
       </Container>
