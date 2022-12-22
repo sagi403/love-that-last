2268cf66b54d84cab1eb6ee0c1c1e791ab61896f
Sagi <sagi403@gmail.com>
2022-12-22 14:48:06 +0200

    Updating the header navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,21 +4,28 @@ import { LinkContainer } from "react-router-bootstrap";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="light" expand="lg" collapseOnSelect>
+      <Navbar bg="light" className="p-2" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>LTL</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i> Cart
-                </Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar>
+            <LinkContainer to="/login" className="mx-3 fs-3">
+              <Nav.Link>
+                <i className="bi bi-person"></i>
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/search" className="mx-3 fs-3">
+              <Nav.Link>
+                <i class="bi bi-search"></i>
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/cart" className="mx-3 fs-3">
+              <Nav.Link>
+                <i class="bi bi-cart"></i>
+              </Nav.Link>
+            </LinkContainer>
+          </Navbar>
         </Container>
       </Navbar>
     </header>
