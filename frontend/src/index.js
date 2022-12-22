3751c1db93d364c0421d0e1b4973739233bfaee1
Sagi <sagi403@gmail.com>
2022-12-22 15:50:57 +0200

    Initial setup for redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./index.css";
 import App from "./App";
 import "./bootstrap.min.css";
@@ -7,9 +9,9 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
