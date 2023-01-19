50ce0bfc023422cf9dd12c5cb9ce5958c06b7a5a
Sagi <sagi403@gmail.com>
2023-01-19 15:55:32 +0200

    Testing the new github workflow

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -52,7 +52,7 @@ const HomeScreen = () => {
       <Container className="py-5">
         <div className="text-center py-5">
           <h1 className="mx-auto header">
-            TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
+            Here At LTL, Our Customers Are Our #1 Priority
           </h1>
           <p className="mx-auto">
             Yes: We love our customers more than anything! And that's saying
