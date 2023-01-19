8af2dd74e2b357ea4b94141870ac2619d56ec406
Sagi <sagi403@gmail.com>
2023-01-19 11:41:17 +0200

    Adding Dockerfile to the backend

--- backend/keys.js
+++ backend/keys.js
@@ -7,7 +7,6 @@ const keys = {
   port: process.env.PORT,
   mongoUri: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
-  paypalAccessToken: process.env.PAYPAL_ACCESS_TOKEN,
   url: process.env.URL,
   emailUser: process.env.EMAIL_USER,
   emailPassword: process.env.EMAIL_PASSWORD,
